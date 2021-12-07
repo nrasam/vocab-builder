8ec816df8d4c427fb31f6a8b89fae9fc9b8a08c7
Novell <nrasam@myseneca.ca>
2021-12-07 16:57:32 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vocab-builder',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
